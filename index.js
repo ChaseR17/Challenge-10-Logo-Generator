feda78c14e493ef4d9283f6f0b04c9d5ea22cd3a
Chase Gilhart <chasegilhart@Chases-MacBook-Pro.local>
2024-06-12 21:35:58 -0400

    gitignore

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
+const SVG = require("./svg");
+const { Circle, Triangle, Square } = require("./shapes");
+const { writeFile } = require("fs/promises");
 
+const generateSVG = () => {
     inquirer.prompt([
         // shape pick
         {
@@ -28,3 +32,36 @@ const inquirer = require("inquirer");
         }
     ])
 
+  // show user response
+  .then((res) => {
+    console.log(res);
+
+    let logoShape;
+
+    //if the response is circle import circle parameter from shapes.js
+    if (res.shape === "Circle") {
+        logoShape = new Circle();
+    }
+    // if the response is triangle import triangle parameter from shapes.js
+    if (res.shape === "Triangle") {
+        logoShape = new Triangle();
+    }
+    // if the response is square import square parameter from shapes.js
+    if (res.shape === "Square") {
+        logoShape = new Square();
+    }
+    // allow user to pick color
+    svgShape.setColor(res.shapeColor)
+
+    // Create a function to generates svg file and to save it in the examples folder
+    const svg = new SVG();
+    svg.setText(res.text, res.textColor);
+    svg.setShape(svgShape);
+    return writeFile("./examples/logo.svg", svg.render());
+}).then(() => {
+    console.log("Generated logo.svg");
+})
+};
+
+// Function call to generates SVG file
+generateSVG();
